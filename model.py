4f44d8c3c584580e107382dc080826ac496e2ec2
AVB934 <avbolagoni@gmail.com>
2026-06-27 11:29:44 +0100

    updated CNN model

--- model.py
+++ model.py
@@ -17,24 +17,25 @@ class SimpleNN(nn.Module):
         return x
 
 class CNN(nn.Module):
+    # input 28x28 1 channel
     def __init__(self, num_classes: int):
         super().__init__()
-        self.conv1 = nn.Conv2d(1,32,3,1,1)
-        self.pool1 = nn.MaxPool2d(2,1)
-        self.conv2 = nn.Conv2d(32,64,3,1,1)
-        self.pool2 = nn.MaxPool2d(2,1)
-
-        self.fc1 = nn.Linear(64*7*7,128)
-        self.fc2 = nn.Linear(128,num_classes)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        
+        #7×7 = spatial dimensions after two stride-2 pooling operations
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, num_classes)
         self.dropout = nn.Dropout(0.5)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(x.size(0),-1) #flatten
+        x = x.view(x.size(0), -1)  # flatten
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
-
         return x
 
