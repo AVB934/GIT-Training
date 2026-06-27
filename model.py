85929b992dd5033693da5d91b537e8745482b821
AVB934 <avbolagoni@gmail.com>
2026-06-27 09:49:40 +0100

    added CNN model to file model.py

--- model.py
+++ model.py
@@ -15,3 +15,26 @@ class SimpleNN(nn.Module):
         x = F.relu(self.fc2(x))
         x = torch.sigmoid(self.fc3(x))
         return x
+
+class CNN(nn.Module):
+    def __init__(self, num_classes: int):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1,32,3,1,1)
+        self.pool1 = nn.MaxPool2d(2,1)
+        self.conv2 = nn.Conv2d(32,64,3,1,1)
+        self.pool2 = nn.MaxPool2d(2,1)
+
+        self.fc1 = nn.Linear(64*7*7,128)
+        self.fc2 = nn.Linear(128,num_classes)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self,x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(x.size(0),-1) #flatten
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
+
