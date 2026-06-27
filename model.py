1659b76f94c944ac3e8d1b69458d5e38a40f69f2
AVB934 <avbolagoni@gmail.com>
2026-06-27 11:24:35 +0100

    updated CNN model

--- model.py
+++ model.py
@@ -19,22 +19,21 @@ class SimpleNN(nn.Module):
 class CNN(nn.Module):
     def __init__(self, num_classes: int):
         super().__init__()
-        self.conv1 = nn.Conv2d(1,32,3,1,1)
-        self.pool1 = nn.MaxPool2d(2,1)
-        self.conv2 = nn.Conv2d(32,64,3,1,1)
-        self.pool2 = nn.MaxPool2d(2,1)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)  # ← stride=2
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.pool2 = nn.MaxPool2d(2, 2)  # ← stride=2
 
-        self.fc1 = nn.Linear(64*7*7,128)
-        self.fc2 = nn.Linear(128,num_classes)
+        self.fc1 = nn.Linear(64*7*7, 128)
+        self.fc2 = nn.Linear(128, num_classes)
         self.dropout = nn.Dropout(0.5)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(x.size(0),-1) #flatten
+        x = x.view(x.size(0), -1)  # flatten
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
-
         return x
 
